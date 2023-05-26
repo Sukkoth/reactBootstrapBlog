68a9b49bc22bbaff475bc9754833b6c211dd1ce6
Sukkoth <suukootj@gmail.com>
2023-05-26 10:21:06 +0300

    remove foregroundColor from content loaders

--- src/Components/ContentLoaders/BlogListLoader.jsx
+++ src/Components/ContentLoaders/BlogListLoader.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const ListLoader = () => (
-    <ContentLoader
-        style={{ padding: '0', margin: '0' }}
-        viewBox='0 10 200 380'
-        foregroundColor='#f5efe6'
-    >
+    <ContentLoader style={{ padding: '0', margin: '0' }} viewBox='0 10 200 380'>
         <rect x='0' y='0' rx='0' ry='0' width='400' height='500' />
     </ContentLoader>
 );
--- src/Components/ContentLoaders/RecentPostsLoader.jsx
+++ src/Components/ContentLoaders/RecentPostsLoader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const SideBarContentLoader = () => (
-    <ContentLoader viewBox='0 0 350 50' foregroundColor='#e3dacc'>
+    <ContentLoader viewBox='0 0 350 50'>
         <rect x='10' y='0' rx='4' ry='4' width='300' height='20' />
         <rect x='10' y='30' rx='3' ry='3' width='150' height='15' />
     </ContentLoader>
